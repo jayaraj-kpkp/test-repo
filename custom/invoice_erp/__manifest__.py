95786eb4868d1d895fd1acb21589c0f5b88dcdf6
jayaraj-kp <jayarajkprajkp@gmail.com>
2025-07-08 11:17:47 +0530

    update

--- custom/invoice_erp/__manifest__.py
+++ custom/invoice_erp/__manifest__.py
@@ -1,3 +1,18 @@
 {
-    'name': "invoice_erp"
+
+    'name': "invoice_erp",
+    'version': '1.0',
+    'summary': "Custom Invoice Template",
+    'description': "This module customizes the invoice layout.",
+    'author': "Your Name",
+    'website': "https://yourcompany.com",
+    'category': 'Accounting',
+    'depends': ['account'],
+    'data': [
+        'views/report_invoice.xml',
+    ],
+    'installable': True,
+    'application': False,
+
+
 }
