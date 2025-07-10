9a9c5b89222d9b1c81335664fc912fec259d5ead
jayaraj-kp <jayarajkprajkp@gmail.com>
2025-07-10 12:42:38 +0530

    add i base

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-exec python odoo-bin -c odoo.conf
+#exec python odoo-bin -c odoo.conf
+#!/bin/bash
+exec python3 odoo-bin -c odoo.conf -d postgresql_odoo18_1 -i base --log-level=debug
 
 
 
