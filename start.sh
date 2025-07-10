c3b6509be45eb7c0ca12626ee8e7a13064c2e0f7
jayaraj-kp <jayarajkprajkp@gmail.com>
2025-07-10 21:57:10 +0530

    up

--- start.sh
+++ start.sh
@@ -1,12 +1,4 @@
-echo "Starting Odoo on Render using PORT=$PORT"
 
-# Update port
-sed -i "s/^http_port = *./http_port = ${PORT}/" odoo.conf
 
-# Check if DB is empty and run base init
-echo "Checking database status..."
-INIT_DB_FLAG_FILE="/opt/render/project/src/.init_done"
+python3 odoo-bin -c odoo.conf -d postgresql_odoo18_new -i base --stop-after-init
 
-python3 odoo-bin -c odoo.conf -d database18 -u all --stop-after-init
-
-exec python3 odoo-bin -c odoo.conf
