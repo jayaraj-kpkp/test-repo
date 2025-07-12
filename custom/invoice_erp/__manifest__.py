638a2a4953b66cc8e1a76de7798e02f16f3dfea8
jayaraj-kp <jayarajkprajkp@gmail.com>
2025-07-12 10:24:24 +0530

    add xml

--- custom/invoice_erp/__manifest__.py
+++ custom/invoice_erp/__manifest__.py
@@ -1,43 +1,43 @@
-# {
-#     'name': "invoice_erp",
-#     'version': '1.0',
-#     'summary': "Custom Invoice Template",
-#     'description': "This module customizes the invoice layout.",
-#     'author': "Your Name",
-#     'website': "https://yourcompany.com",
-#     'category': 'Accounting',
-#     'depends': ['account'],
-#     'data': [
-#         'views/report_invoice.xml',
-#     ],
-#     'assets': {
-#         'web.report_assets_common': [
-#             'invoice_erp/static/src/css/invoice_styles.css',
-#         ],
-#     },
-#     'installable': True,
-#     'application': False
-# }
 {
-    "name": "Custom Invoice Layout",
-    "version": "1.0",
-    "summary": "Customized invoice template with bilingual header, QR code, and bank details",
-    "description": """
-        This module customizes the standard invoice PDF layout to match the company's requirements,
-        including Arabic-English headers, QR code generation, and custom styling.
-    """,
-    "category": "Accounting",
-    "author": "Your Company Name",
-    "website": "https://yourcompanywebsite.com",
-    "license": "LGPL-3",
-    "depends": [
-        "account",
+    'name': "invoice_erp",
+    'version': '1.0',
+    'summary': "Custom Invoice Template",
+    'description': "This module customizes the invoice layout.",
+    'author': "Your Name",
+    'website': "https://yourcompany.com",
+    'category': 'Accounting',
+    'depends': ['account'],
+    'data': [
+        'views/report_invoice.xml',
     ],
-    "data": [
-        "views/report_invoice.xml",
-    ],
-    "assets": {},
-    "installable": True,
-    "application": False,
-    "auto_install": False,
+    # 'assets': {
+    #     'web.report_assets_common': [
+    #         'invoice_erp/static/src/css/invoice_styles.css',
+    #     ],
+    # },
+    'installable': True,
+    'application': False
 }
+# {
+#     "name": "Custom Invoice Layout",
+#     "version": "1.0",
+#     "summary": "Customized invoice template with bilingual header, QR code, and bank details",
+#     "description": """
+#         This module customizes the standard invoice PDF layout to match the company's requirements,
+#         including Arabic-English headers, QR code generation, and custom styling.
+#     """,
+#     "category": "Accounting",
+#     "author": "Your Company Name",
+#     "website": "https://yourcompanywebsite.com",
+#     "license": "LGPL-3",
+#     "depends": [
+#         "account",
+#     ],
+#     "data": [
+#         "views/report_invoice.xml",
+#     ],
+#     "assets": {},
+#     "installable": True,
+#     "application": False,
+#     "auto_install": False,
+# }
