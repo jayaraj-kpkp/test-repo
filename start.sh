54baaf7b42bdc9ab728382535e483d9c4c4e777b
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-07-14 12:50:50 +0530

    add i base

--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-exec python3 odoo-bin -c odoo.conf -d saudi_sales_db -i sale,account,sale_management,l10n_sa --stop-after-init
-
-
+exec  python3 odoo-bin -c odoo.conf -d saudi_arabia_db -i base --stop-after-init
 
