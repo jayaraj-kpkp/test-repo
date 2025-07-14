5f42121aef1277c500adc8bebb39ff1af875eccb
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-07-14 12:08:36 +0530

    install sales

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-exec python3 odoo-bin -c odoo.conf --http-port=$PORT
+exec python3 odoo-bin -c odoo.conf -d saudi_sales_db -i sale,account,sale_management,l10n_sa --stop-after-init
+
 
 
