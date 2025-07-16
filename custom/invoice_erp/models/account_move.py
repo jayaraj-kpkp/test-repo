ef21567b392cc90ed1dfa8dddb1a546ccd92efe1
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-07-16 14:54:29 +0530

    add account move new code

--- custom/invoice_erp/models/account_move.py
+++ custom/invoice_erp/models/account_move.py
@@ -1,10 +1,12 @@
+from odoo import models, fields
 import base64
 import qrcode
 from io import BytesIO
-from odoo import models
-from odoo import models, fields
+
+
 class AccountMove(models.Model):
     _inherit = 'account.move'
+
     qr_code_image = fields.Binary(
         string="ZATCA QR Code",
         compute="_compute_qr_code_image",
@@ -17,23 +19,22 @@ class AccountMove(models.Model):
 
     def _generate_qr_code(self):
         self.ensure_one()
-        # ZATCA Tag Encoding:
-        # Tag 1: Seller Name
-        # Tag 2: VAT Number
-        # Tag 3: Invoice Date (ISO8601)
-        # Tag 4: Invoice Total
-        # Tag 5: VAT Total
 
+        # Helper to encode a single TLV tag
         def _encode_tag(tag, value):
             value_bytes = value.encode('utf-8')
             return bytes([tag]) + bytes([len(value_bytes)]) + value_bytes
 
         seller_name = self.company_id.name or ""
         seller_vat = self.company_id.vat or ""
-        invoice_date = self.invoice_date.strftime('%Y-%m-%dT%H:%M:%S') if self.invoice_date else ""
+        invoice_date = (
+            self.invoice_date.strftime('%Y-%m-%dT%H:%M:%S')
+            if self.invoice_date else ""
+        )
         invoice_total = "{0:.2f}".format(self.amount_total)
         vat_total = "{0:.2f}".format(self.amount_tax)
 
+        # Build the TLV payload
         qr_bytes = (
             _encode_tag(1, seller_name) +
             _encode_tag(2, seller_vat) +
@@ -41,8 +42,10 @@ class AccountMove(models.Model):
             _encode_tag(4, invoice_total) +
             _encode_tag(5, vat_total)
         )
+        # Encode TLV to Base64
         qr_base64 = base64.b64encode(qr_bytes).decode('utf-8')
 
+        # Generate QR code image
         qr = qrcode.QRCode(
             version=1,
             error_correction=qrcode.constants.ERROR_CORRECT_M,
@@ -55,4 +58,6 @@ class AccountMove(models.Model):
         img = qr.make_image(fill_color="black", back_color="white")
         buffer = BytesIO()
         img.save(buffer, format='PNG')
-        return base64.b64encode(buffer.getvalue())
+
+        # Return Base64 string for embedding in QWeb
+        return base64.b64encode(buffer.getvalue()).decode()
