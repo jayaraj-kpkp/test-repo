9ed519b62fbc979157b8d6d28dc7f905e272e1f5
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-07-19 14:07:30 +0530

    remove xml

--- odoo/addons/sale_management/__manifest__.py
+++ odoo/addons/sale_management/__manifest__.py
@@ -43,7 +43,7 @@ The Dashboard for the Sales Manager will include
         'security/ir.model.access.csv',
         'security/sale_management_security.xml',
 
-        'report/report_quotation_template.xml.xml',
+        'report/report_quotation_template.xml',
 
         # Define SO template views & actions before their place of use
         'views/sale_order_template_views.xml',
