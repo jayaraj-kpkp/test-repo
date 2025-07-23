d0522fc93f966d2d0f4345a95349061297260b1a
jayaraj-kpkp <jayarajkpkp@gmail.com>
2025-07-23 16:44:58 +0530

    add code for vendor

---
+++ custom/custom_vendor_bill/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Custom Vendor Bill Report",
+    "version": "1.0",
+    "depends": ["account"],
+    "author": "Your Company",
+    "category": "Accounting",
+    "summary": "Custom Vendor Bill Print Template",
+    "data": [
+        "views/vendor_bill_report_action.xml",
+        "report/vendor_bill_template.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
